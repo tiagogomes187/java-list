6644b3914cfc0d4a71e6b988638bc2901655b18f
Tiago Gomes <tiagogomes187@gmail.com>
2022-10-26 20:31:33 -0300

    Project Created

---
+++ src/com/github/tiagogomes187/Ex01/TemperaturaMes.java
@@ -0,0 +1,23 @@
+package com.github.tiagogomes187.Ex01;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+/*Faça um programa que receba a temperatura média os 6 primeiros meses do ano e armazene-as em uma lista.
+Após isto, calcule a média semestral das temperaturas e mostre todas as temperaturas acima desta média,
+e em que mês elas ocorreram (mostrar o mês por extenso: 1 – Janeiro, 2 – Fevereiro e etc).
+*/
+
+
+public class TemperaturaMes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Double> temperaturas = new ArrayList<Double>();
+
+        //
+
+        sc.close();
+    }
+}
